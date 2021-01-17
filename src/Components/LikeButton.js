03cb9a7379e0f55c513e588d985fe624a57058e1
Pradeep Kumar <45354395+pradeepp88@users.noreply.github.com>
2021-01-17 18:52:16 -0500

    add LikeButton and edited dependencies

--- src/Components/LikeButton.js
+++ src/Components/LikeButton.js
@@ -1,14 +1,22 @@
 import React from "react";
 
 class LikeButton extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    // }
+    state = { 
+        counter: 0
+     }
+    
 
+    handleClick = (e)=>{
+        e.preventDefault();
+        let counter=this.state.counter;
+        counter = counter + 1;
+        this.setState({counter: counter})
+    }
     render() { 
-
         return ( 
-           <></>
+            <>
+            {` `}<button onClick={this.handleClick}>Like</button> {this.state.counter} Likes
+            </>
          );
     }
 }
