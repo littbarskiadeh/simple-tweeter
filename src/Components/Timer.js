bcc0f0c07805842fbeb945f9f6b73e08c56857b2
littbarski <lit.adeh@gmail.com>
2021-01-17 02:11:45 -0500

    added comment feature, refactored to functional components

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -1,17 +1,14 @@
 import React from "react";
 import Moment from "react-moment";
 
-class Timer extends React.Component {
-
-    render() { 
-        return ( 
-            <>
-                <Moment format="hh:mm:ss" style={{margin: '15px'}}>
-                    {Moment.moment }  
-                </Moment>
-            </>
-         );
-    }
+const Timer = () => {
+    return ( 
+        <>
+            <Moment format="hh:mm:ss" style={{margin: '15px'}}>
+                {Moment.moment }  
+            </Moment>
+        </>
+    );
 }
  
 export default Timer;
